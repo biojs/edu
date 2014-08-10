444ecb28464a8d5a412d4bfc89643b01ba031310
Sebastian Wilzbach <sebi@wilzbach.me>
2014-08-10 18:31:28 +0200

    awesome code block plugin

--- _plugins/highlight.rb
+++ _plugins/highlight.rb
@@ -1,49 +1,100 @@
-# Example:
-# {% blockhighlight challenge %} 
-# foo
-# {% endblockhighlight %}
-
 module Jekyll
-    class HighlightBlock< Liquid::Block
+  module Tags
+    class SpecialHighlightBlock < Liquid::Block
+      include Liquid::StandardFilters
 
-      def initialize(tag_name, block_name, tokens)
+      # The regular expression syntax checker. Start with the language specifier.
+      # Follow that by zero or more space separated options that take one of two
+      # forms:
+      #
+      # 1. name
+      # 2. name=value
+      SYNTAX = /^([a-zA-Z0-9.+#-]+)((\s+\w+(=\w+)?)*)$/
+
+      def initialize(tag_name, markup, tokens)
         super
-        @block_name = block_name.strip
+        if markup.strip =~ SYNTAX
+          @lang = $1
+          @options = {}
+          @collapsible = ""
+
+          if defined?($2) && $2 != ''
+            $2.split.each do |opt|
+              key, value = opt.split('=')
+              if value.nil?
+                if key == 'linenos'
+                  value = 'inline'
+                else
+                  value = true
+                end
+              end
+              @options[key] = value
+            end
+          end
+
+          if defined? @options['collapsible']
+            @collapsible = "collapsible"
+          end
+
+        else
+          raise SyntaxError.new <<-eos
+Syntax Error in tag 'highlight' while parsing the following markup:
+
+  #{markup}
+
+Valid syntax: highlight <lang> [linenos collapsible=true]
+eos
+        end
       end
 
       def render(context)
-        site = context.registers[:site]
-        converter = site.getConverterImpl(::Jekyll::Converters::Markdown)
-        output = converter.convert(super(context))
-        strGlyph = '';  
-        
-          
-        case @block_name 
-            when "challenge"
-                strGlyph = 'glyphicon glyphicon-road';
-            
-            when "verification"
-                strGlyph = 'glyphicon glyphicon-check';
-            
-            when "stop"
-                strGlyph = 'glyphicon glyphicon-minus-sign';
-            
-            when "question"
-                strGlyph = 'glyphicon glyphicon-bell';
-            
-            when "raise"
-                strGlyph = 'glyphicon glyphicon-hand-up';
-                
-            end
-          
-        strReturn =  '<div style="display:block"><div class="tutorials-highlight" style="display:block;"><span class="' + strGlyph + '" style="float:left;display:block;margin-right:20px"></span><div class="tutorials-highlight-text">'  \
-                    + output \
-                    + '</div></div></div>';
-        return strReturn;
-            
+        if context.registers[:site].pygments
+          render_pygments(context, super)
+        else
+          render_codehighlighter(context, super)
+        end
       end
+
+      def render_pygments(context, code)
+        require 'pygments'
+
+        @options[:encoding] = 'utf-8'
+
+        classes = Array[@lang]
+        if defined? @collapsible
+          classes.push "collapsible"
+          puts "collapsible"
+        end
+
+        output = add_code_tags(
+          Pygments.highlight(code, :lexer => @lang, :options => @options),
+          classes
+        )
+
+        output = context["pygments_prefix"] + output if context["pygments_prefix"]
+        output = output + context["pygments_suffix"] if context["pygments_suffix"]
+        output
+      end
+
+      def render_codehighlighter(context, code)
+        #The div is required because RDiscount blows ass
+
+        <<-HTML
+  <div>
+    <pre><code class='#{@lang} #{@collapsible}'>#{h(code).strip}</code></pre>
+  </div>
+        HTML
+      end
+
+      # classes is an array
+      def add_code_tags(code, classes)
+        # Add nested <code> tags to code blocks
+        code = code.sub(/<pre>/,'<pre><code class="' + classes.join(' ') + '">')
+        code = code.sub(/<\/pre>/,"</code></pre>")
+      end
+
     end
+  end
 end
 
-
-Liquid::Template.register_tag('highlightblock', Jekyll::HighlightBlock)
+Liquid::Template.register_tag('code', Jekyll::Tags::SpecialHighlightBlock)
---
+++ _plugins/iconify.rb
@@ -0,0 +1,49 @@
+# Example:
+# {% blockhighlight challenge %} 
+# foo
+# {% endblockhighlight %}
+
+module Jekyll
+    class HighlightBlock< Liquid::Block
+
+      def initialize(tag_name, block_name, tokens)
+        super
+        @block_name = block_name.strip
+      end
+
+      def render(context)
+        site = context.registers[:site]
+        converter = site.getConverterImpl(::Jekyll::Converters::Markdown)
+        output = converter.convert(super(context))
+        strGlyph = '';  
+        
+          
+        case @block_name 
+            when "challenge"
+                strGlyph = 'glyphicon glyphicon-road';
+            
+            when "verification"
+                strGlyph = 'glyphicon glyphicon-check';
+            
+            when "stop"
+                strGlyph = 'glyphicon glyphicon-minus-sign';
+            
+            when "question"
+                strGlyph = 'glyphicon glyphicon-bell';
+            
+            when "raise"
+                strGlyph = 'glyphicon glyphicon-hand-up';
+                
+            end
+          
+        strReturn =  '<div style="display:block"><div class="tutorials-highlight" style="display:block;"><span class="' + strGlyph + '" style="float:left;display:block;margin-right:20px"></span><div class="tutorials-highlight-text">'  \
+                    + output \
+                    + '</div></div></div>';
+        return strReturn;
+            
+      end
+    end
+end
+
+
+Liquid::Template.register_tag('highlightblock', Jekyll::HighlightBlock)
